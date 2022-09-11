aa7d9a9aa3791f53d7ef5a4d22a87cc7e77d4c1a
gerchek <gerchekgerchek1@gmail.com>
2022-09-11 04:48:17 +0000

    server updated

--- app/internal/setup/routes/routes.go
+++ app/internal/setup/routes/routes.go
@@ -6,6 +6,7 @@ import (
 	"rss/internal/services/rss/constructor"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 )
 
 type MyRoute struct {
@@ -14,5 +15,10 @@ type MyRoute struct {
 func (rr *MyRoute) Routes() {
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/posts", constructor.RssController.GetAll)
-	log.Fatal(http.ListenAndServe("95.85.124.41:8080", r))
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With","Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	
+	log.Fatal(http.ListenAndServe("95.85.124.41:8080", handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 }
